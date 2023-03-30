b9fe952d78a103591a5597008dcd2ee7e7002bcc
Yuuta1467 <s1341011we@gmail.com>
2023-03-30 10:11:05 +0000

    [end] 3-1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create]
+  end
+
   resources :users, only: [:index,:show,:edit,:update]
 
   get "home/about"=>"homes#about"
