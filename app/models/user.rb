2a35336eea4f38c496304a17f6d6185cd3865b66
Yuuta1467 <s1341011we@gmail.com>
2023-04-01 08:33:48 +0000

    [end] 4-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-　has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
@@ -27,7 +25,6 @@ class User < ApplicationRecord
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
@@ -35,4 +32,5 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
@@ -8,13 +10,9 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update] do
-    member do
-      get :follows, :followers
-    end
+  resources :users, only: [:index,:show,:edit,:update]
 
-    resource :relationships, only: [:create, :destroy]
-  end
+  resource :relationships, only: [:create, :destroy]
 
   get "home/about"=>"homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
