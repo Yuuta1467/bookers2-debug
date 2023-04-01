ff22d48aa35c1499578d5e9e7caaa8fb60b71353
Yuuta1467 <s1341011we@gmail.com>
2023-04-01 08:01:59 +0000

    [end] 4-1

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+      get :follows, :followers
+    end
+
+    resource :relationships, only: [:create, :destroy]
+  end
 
   get "home/about"=>"homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
