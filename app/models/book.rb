a5377924f92a238994ce27b41210c736a368cc91
Yuuta1467 <s1341011we@gmail.com>
2023-04-03 07:45:18 +0000

    [end2] 6-5

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
 
-  has_one_attached :image
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -12,18 +11,15 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @book = Book.where("title LIKE?","#{word}")
-    elsif search == "forward_match"
-      @book = Book.where("title LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @book = Book.where("title LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @book = Book.where("title LIKE?","%#{word}%")
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE ?', content+'%')
+    elsif method == 'backward'
+      Book.where('title LIKE ?', '%'+content)
     else
-      @book = Book.all
+      Book.where('title LIKE ?', '%'+content+'%')
     end
   end
-
 end
