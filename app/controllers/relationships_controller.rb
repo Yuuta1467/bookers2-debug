db6ffeec6540c78fc98703cc1a6d5fcb7ef685d0
Yuuta1467 <s1341011we@gmail.com>
2023-04-03 08:33:47 +0000

    [end2] 6-7

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,10 @@
 class RelationshipsController < ApplicationController
-　before_action :authenticate_user!
-
-　def create
-　  user = User.find(params[:user_id])
-　  current_user.follow(user)
-　  redirect_to request.referer
-　end
+  before_action :authenticate_user!
+  def create
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+		redirect_to request.referer
+  end
 
   def destroy
     user = User.find(params[:user_id])
